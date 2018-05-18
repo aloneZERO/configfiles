70c3877a87cd23be89758f3a842279e4c8d9605a
aloneZERO <alonezero@foxmail.com>
2018-05-18 12:50:11 +0800

    update hadoop/ssh_login.sh

--- hadoop/script/ssh_login.sh
+++ hadoop/script/ssh_login.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-ssh_login(){
-    case $1 in
-        1) printf "Starting login hadoop@172.31.151.$2\n"
-            ssh hadoop@172.31.151.$2
-        ;;
-        2) printf "Starting login hadoop@10.0.1.$2\n"
-            ssh hadoop@10.0.1.$2
-        ;;
-        o) printf "Starting login other@172.31.233.$2\n"
-            ssh iii@172.31.233.$2
-        ;;
-        h) printf "ssh_login 1 <n>: ssh hadoop@172.31.151.<n>\n"
-           printf "ssh_login 2 <n>: ssh hadoop@10.0.1.<n>\n"
-           printf "ssh_login o <n>: ssh other@172.31.233.<n>\n"
-        ;;
-    esac
-}
-
-export -f ssh-login
+case $1 in
+    1) printf "Starting login hadoop@172.31.151.$2\n"
+        ssh hadoop@172.31.151.$2
+    ;;
+    2) printf "Starting login hadoop@10.0.1.$2\n"
+        ssh hadoop@10.0.1.$2
+    ;;
+    o) printf "Starting login other@172.31.233.$2\n"
+        ssh iii@172.31.233.$2
+    ;;
+    h) printf "ssh_login 1 <n>: ssh hadoop@172.31.151.<n>\n"
+       printf "ssh_login 2 <n>: ssh hadoop@10.0.1.<n>\n"
+       printf "ssh_login o <n>: ssh other@172.31.233.<n>\n"
+    ;;
+esac
